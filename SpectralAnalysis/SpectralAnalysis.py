39652b623787945bfc4ae10af96247de58933e14
Ingo Scholtes <ingos@gmxpro.de>
2015-06-01 17:58:35 +0200

    updated graphics, added videos

--- SpectralAnalysis/SpectralAnalysis.py
+++ SpectralAnalysis/SpectralAnalysis.py
@@ -44,9 +44,34 @@ def plotSpectrum(t, filename='spectrum'):
     plt.savefig(filename)
     plt.close()
 
-def plotFiedler(t, filename='fiedler'):
-    fiedler = tn.Measures.FiedlerVector(t)
-    fiedler_null = tn.Measures.FiedlerVector(t, model= 'NULL')
+def plotFiedler(t, filename='fiedler', model='SECOND', col='b'):
+    fiedler = tn.Measures.FiedlerVector(t, model)
+    #fiedler_null = tn.Measures.FiedlerVector(t, model= 'NULL')
+    if model == 'NULL':
+        fiedler = list(reversed(fiedler))
+    plt.clf()
+    plt.tick_params(axis='both', which='major', labelsize=20)
+    #plt.xlabel('$i$', fontsize=20)
+    #plt.ylabel(r'$(v_2)_i$', fontsize=20)
+    plt.xlim(0, len(fiedler))    
+    #plt.subplots_adjust(bottom=0.15)
+    #plt.subplots_adjust(left=0.15)
+    plt.scatter(range(len(fiedler)), np.real(fiedler), color=col, s=80)
+    plt.tight_layout()
+    #plt.scatter(range(len(fiedler_null)), np.real(fiedler_null), color="b")
+    plt.savefig(filename)
+    plt.close()
+
+def plotVector(t, filename='vector', i=2):
+    L = tn.Laplacian(t)
+    w, v = spl.eig(L, left=True, right=False)
+
+    L_null = tn.Laplacian(t, model='NULL')
+    w_null, v_null = spl.eig(L_null, left=True, right=False)
+
+    vector_null = v_null[:,np.argsort(np.absolute(w_null))][:,i]
+    vector = v[:,np.argsort(np.absolute(w))][:,i]
+
     plt.clf()
     plt.tick_params(axis='both', which='major', labelsize=20)
     plt.tick_params(axis='both', which='minor', labelsize=20)       
@@ -54,8 +79,8 @@ def plotFiedler(t, filename='fiedler'):
     plt.ylabel(r'$(v_2)_i$', fontsize=30)
     plt.subplots_adjust(bottom=0.25)
     plt.subplots_adjust(left=0.25)
-    plt.scatter(range(len(fiedler)), np.real(fiedler), color="r")
-    plt.scatter(range(len(fiedler_null)), np.real(fiedler_null), color="b")
+    plt.scatter(range(len(vector)), np.real(vector), color="r")
+    plt.scatter(range(len(vector_null)), np.real(vector_null), color="b")
     plt.savefig(filename)
     plt.close()
 
@@ -74,7 +99,7 @@ visual_style["edge_width"] = [np.log(x)/4 for x in g1.es()["weight"]]
 visual_style["vertex_color"] = "lightblue"
 visual_style["edge_curved"] = .5
 visual_style["edge_arrow_size"] = 0.4
-visual_style["vertex_size"] = 10
+visual_style["vertex_size"] = 15
 visual_style["layout"] = g1.layout_auto()
 
 igraph.plot(g1, 'LT_g1.png', **visual_style)
@@ -83,6 +108,9 @@ igraph.plot(g1, 'LT_g1.pdf', **visual_style)
 # Export video of diffusion dynamics
 tn.exportDiffusionMovieFramesFirstOrder(t_lt, 'frames/LT_diffusion_t2', visual_style, steps = 200, initial_index=0, model='SECOND')
 tn.exportDiffusionMovieFramesFirstOrder(t_lt, 'frames/LT_diffusion_t1', visual_style, steps = 200, initial_index=0, model='NULL')
+
+# Export side-by-side comparison
+tn.exportDiffusionComparisonVideo(t_lt, 'LT_diffusion_comparison.mp4', visual_style, steps = 200, initial_index=0)
 
 # Export video of empirical and shuffled temporal evolution
 visual_style["vertex_color"] = "lightblue"
@@ -113,6 +141,16 @@ print('lambda_2(L_null) =', tn.AlgebraicConn(t_lt, model='NULL'))
 
 plotFiedler(t_lt, 'LT_fiedler.pdf')
 plotSpectrum(t_lt, 'LT_spectrum.pdf')
+
+L = tn.Laplacian(t_lt)
+np.linalg.matrix_rank(L)
+
+L_n = tn.Laplacian(t_lt, model='NULL')
+np.linalg.matrix_rank(L_n)
+
+plotVector(t_lt, 'LT_v4.pdf', i=3)
+plotVector(t_lt, 'LT_v5.pdf', i=4)
+plotVector(t_lt, 'LT_v6.pdf', i=5)
 
 # Verify that leading eigenvector of both is the same
 L = tn.Laplacian(t_lt)
@@ -159,6 +197,9 @@ igraph.plot(g1, 'AN_g1.pdf', **visual_style)
 tn.exportDiffusionMovieFramesFirstOrder(t_an, 'frames/AN_diffusion_t2', visual_style, steps = 200, initial_index=0, model='SECOND')
 tn.exportDiffusionMovieFramesFirstOrder(t_an, 'frames/AN_diffusion_t1', visual_style, steps = 200, initial_index=0, model='NULL')
 
+# Export side-by-side comparison
+tn.exportDiffusionComparisonVideo(t_an, 'AN_diffusion_comparison.mp4', visual_style, steps = 200, initial_index=0)
+
 # Export video of temporal evolution
 visual_style["vertex_color"] = "lightblue"
 t_an.exportMovieFrames('frames/AN', visual_style, realtime=False, maxSteps=400)
@@ -174,7 +215,7 @@ visual_style["edge_width"] = [x**.1 for x in g2n.es()["weight"]]
 visual_style["edge_arrow_size"] = 0.4
 visual_style["vertex_color"] = "lightblue"
 visual_style["edge_curved"] = .2
-visual_style["vertex_size"] = 8
+visual_style["vertex_size"] = 15
 visual_style["layout"] = g2n.layout_auto()
 igraph.plot(g2n, 'AN_g2n.pdf', **visual_style)
 igraph.plot(g2n, 'AN_g2n.png', **visual_style)
@@ -205,7 +246,7 @@ visual_style["layout"] = g2.layout_auto()
 visual_style["edge_width"] = [x**.5 for x in g2.es()["weight"]]
 visual_style["edge_arrow_size"] = 0.05
 visual_style["vertex_color"] = "lightblue"
-visual_style["vertex_size"] = 5
+visual_style["vertex_size"] = 8
 igraph.plot(g2, 'RM_g2.pdf', **visual_style)
 igraph.plot(g2, 'RM_g2.png', **visual_style)
 
@@ -214,12 +255,15 @@ g1 = t_rm.igraphFirstOrder()
 
 visual_style["edge_width"] = [np.log(x)/4 for x in g1.es()["weight"]]
 visual_style["layout"] = g1.layout_auto()
+visual_style["vertex_size"] = 15
 igraph.plot(g1, 'RM_g1.pdf', **visual_style)
 igraph.plot(g1, 'RM_g1.png', **visual_style)
 
 # Export diffusion video
 tn.exportDiffusionMovieFramesFirstOrder(t_rm, 'frames/RM_diffusion_t2', visual_style, steps = 200, initial_index=0, model='SECOND')
 tn.exportDiffusionMovieFramesFirstOrder(t_rm, 'frames/RM_diffusion_t1', visual_style, steps = 200, initial_index=0, model='NULL')
+
+tn.exportDiffusionComparisonVideo(t_rm, 'RM_diffusion_comparison.mp4', visual_style, steps = 200, initial_index=0)
 
 # Spectral analysis
 print('lambda_2(L) =', tn.AlgebraicConn(t_rm))
@@ -281,46 +325,60 @@ plotSpectrum(t_em, 'EM_spectrum.pdf')
 #  Model data
 ################################
 t_su = tn.TemporalNetwork.readFile('../../pyTempNetDemo/data/sigma0_75.trigram', sep= ' ', fformat='TRIGRAM')
+t_sd = tn.TemporalNetwork.readFile('../../pyTempNetDemo/data/sigma-0_75.trigram', sep= ' ', fformat='TRIGRAM')
 
 # Plot second-order network
-g2 = t_su.igraphSecondOrder()
+g2_su = t_su.igraphSecondOrder()
 visual_style = {}
-visual_style["layout"] = g2.layout_auto()
-visual_style["edge_width"] = [x**.5 for x in g2.es()["weight"]]
+visual_style["layout"] = g2_su.layout_kamada_kawai()
+visual_style["edge_width"] = [x*5 for x in g2_su.es()["weight"]]
 visual_style["edge_arrow_size"] = 0.2
+visual_style["edge_curved"] = 0.25
 visual_style["vertex_color"] = "lightblue"
 visual_style["vertex_size"] = 10
-igraph.plot(g2, 'Model_075_g2.pdf', **visual_style)
+igraph.plot(g2_su, 'Model_su_g2.pdf', **visual_style)
+
+g2_sd = t_sd.igraphSecondOrder()
+visual_style["edge_width"] = [x*5 for x in g2_sd.es()["weight"]]
+igraph.plot(g2_sd, 'Model_sd_g2.pdf', **visual_style)
+
+g2_sd_null = t_sd.igraphSecondOrderNull()
+visual_style["edge_width"] = [x*50 for x in g2_sd_null.es()["weight"]]
+igraph.plot(g2_sd_null, 'Model_null_g2.pdf', **visual_style)
+
 
 # Plot first-order network
-g1 = t.igraphFirstOrder()
+g1 = t_su.igraphFirstOrder()
 
-visual_style["edge_width"] = [np.log(x)/4 for x in g1.es()["weight"]]
-visual_style["layout"] = g1.layout_auto()
-igraph.plot(g1, 'Model_g1.pdf', **visual_style)
+visual_style = {}
+visual_style["edge_width"] = [x for x in g1.es()["weight"]]
+visual_style["layout"] = g1.layout_kamada_kawai()
+visual_style["vertex_color"] = "lightblue"
+visual_style["vertex_size"] = 20
+visual_style["edge_curved"] = .25
+igraph.plot(g1.as_undirected(), 'Model_g1.pdf', **visual_style)
 
 # Export diffusion videos
 tn.exportDiffusionMovieFramesFirstOrder(t_su, 'frames/Model_075_diffusion_t2', visual_style, steps = 200, initial_index=50, model='SECOND')
 tn.exportDiffusionMovieFramesFirstOrder(t_su, 'frames/Model_075_diffusion_t1', visual_style, steps = 200, initial_index=50, model='NULL')
+tn.exportDiffusionMovieFramesFirstOrder(t_sd, 'frames/Model_-075_diffusion_t2', visual_style, steps = 200, initial_index=50, model='SECOND')
 
 # Spectral analysis
-print('lambda_2(L) =', tn.AlgebraicConn(t_su))
-print('lambda_2(L_null) =', tn.AlgebraicConn(t_su, model='NULL'))
+print('lambda_2(L_su) =', tn.AlgebraicConn(t_su))
+print('lambda_2(L_sd) =', tn.AlgebraicConn(t_sd))
 
-plotFiedler(t_su, 'Model_su_fiedler.pdf')
-plotSpectrum(t_su, 'Model_su_spectrum.pdf')
+plotFiedler(t_su, 'Model_su_fiedler.pdf', col='g')
+plotFiedler(t_su, 'Model_null_fiedler.pdf', model='NULL', col='b')
+plotFiedler(t_sd, 'Model_sd_fiedler.pdf', col='r')
 
-# slow-down
-t_sd = tn.TemporalNetwork.readFile('../../pyTempNetDemo/data/sigma-0_75.trigram', sep= ' ', fformat='TRIGRAM')
+plotSpectrum(t_su, 'Model_su_spectrum.pdf', col='r')
 
-# Export diffusion video
-tn.exportDiffusionMovieFramesFirstOrder(t_sd, 'frames/Model_-075_diffusion_t2', visual_style, steps = 200, initial_index=50, model='SECOND')
 
 # Spectral analysis
 print('lambda_2(L) =', tn.AlgebraicConn(t_sd))
 print('lambda_2(L_null) =', tn.AlgebraicConn(t_sd, model='NULL'))
 
-plotFiedler(t_sd, 'Model_sd_fiedler.pdf')
+
 plotSpectrum(t_sd, 'Model_sd_spectrum.pdf')
 
 
